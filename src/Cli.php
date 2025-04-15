e9f2cfcc873446c88d19e3b8287bafc8e83fb6dd
имя фамилия < >
2025-04-15 10:38:20 +0400

    Initial commit

--- src/Cli.php
+++ src/Cli.php
@@ -2,11 +2,12 @@
 
 namespace PhpProject45\Cli;
 
+use function cli\line;
+use function cli\prompt;
+
 function greetUser() {
-    echo "Welcome to the Brain Games!\n";
-    echo "May I have your name?\n";
-    
-    $name = trim(fgets(STDIN));
-    echo "Hello, {$name}!\n";
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
 
