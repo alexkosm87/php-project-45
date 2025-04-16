fd10780e9829a904c61a06134c73e89f0c4404f0
alexkosm87 <kosm24@yandex.ru>
2025-04-16 11:11:38 +0400

    Update Calculator.php

--- src/Games/Calculator.php
+++ src/Games/Calculator.php
@@ -1,1 +1,27 @@
+<?php
+
+namespace Brain\Games;
+
+class Calculator
+{
+    public function getQuestion()
+    {
+        $num1 = rand(1, 50);
+        $num2 = rand(1, 50);
+        $operation = $this->getRandomOperation();
+        return "$num1 $operation $num2";
+    }
+
+    public function getCorrectAnswer($question)
+    {
+        eval('$result = ' . $question . ';');
+        return $result;
+    }
+
+    private function getRandomOperation()
+    {
+        $operations = ['+', '-', '*'];
+        return $operations[array_rand($operations)];
+    }
+}
 
