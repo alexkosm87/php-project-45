cd3943644fc5770e06774386ffd3af42d9ae7a83
alexkosm87 <kosm24@yandex.ru>
2025-04-16 11:34:26 +0400

    Update and rename Calculator.php to Calc.php

---
+++ src/Games/Calc.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace PhpProject45\Games;
+
+use PhpProject45\Engine;
+
+class Calc
+{
+    private const OPERATIONS = ['+', '-', '*'];
+    private const ROUNDS = 3;
+
+    public function run()
+    {
+        $name = Engine::getUserName();
+        Engine::welcome($name);
+
+        for ($i = 0; $i < self::ROUNDS; $i++) {
+            $question = $this->generateQuestion();
+            $correctAnswer = $this->calculateAnswer($question);
+
+            Engine::askQuestion($question);
+            $userAnswer = Engine::getUserAnswer();
+
+            if ($userAnswer != $correctAnswer) {
+                Engine::wrongAnswer($userAnswer, $correctAnswer, $name);
+                return;
+            }
+
+            Engine::correctAnswer();
+        }
+
+        Engine::congratulate($name);
+    }
+
+    private function generateQuestion(): string
+    {
+        $num1 = rand(1, 50);
+        $num2 = rand(1, 50);
+        $operation = self::OPERATIONS[array_rand(self::OPERATIONS)];
+        
+        return "$num1 $operation $num2";
+    }
+
+    private function calculateAnswer(string $question): int
+    {
+        return eval("return $question;");
+    }
+}
+
+
