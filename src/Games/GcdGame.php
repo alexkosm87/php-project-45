cdf2f27f463011188a4e1819e4f83785ebce65ee
alexkosm87 <kosm24@yandex.ru>
2025-04-18 09:22:26 +0400

    Update GcdGame.php

--- src/Games/GcdGame.php
+++ src/Games/GcdGame.php
@@ -1,44 +1,41 @@
 <?php
 
-Project45\Games;
+namespace PhpProject45\Games;
 
 use PhpProject45\Engine;
 
-class GcdGame
+function runGcdGame(string $name): void
 {
-    public function gcd(int $a, int $b): int
-    {
-        while ($b !== 0) {
-            $temp = $b;
-            $b = $a % $b;
-            $a = $temp;
-        }
-        return $a;
-    }
+    const ROUNDS = 3;
 
-    public function run(): void
-    {
-        $name = Engine::getUserName();
-        Engine::welcome($name);
-        echo "Find the greatest common divisor of given numbers.\n";
+    echo "Find the greatest common divisor of given numbers.\n";
 
-        for ($i = 0; $i < 3; $i++) {
-            $num1 = random_int(1, 100);
-            $num2 = random_int(1, 100);
-            $correctAnswer = $this->gcd($num1, $num2);
+    for ($i = 0; $i < ROUNDS; $i++) {
+        $num1 = random_int(1, 100);
+        $num2 = random_int(1, 100);
+        $correctAnswer = gcd($num1, $num2);
 
-            $question = "$num1 $num2";
-            Engine::askQuestion($question);
-            $userAnswer = Engine::getUserAnswer();
+        $question = "$num1 $num2";
+        Engine\askQuestion($question);
+        $userAnswer = Engine\getUserAnswer();
 
-            if ($userAnswer !== (string)$correctAnswer) { // Приведение к строке для сравнения
-                Engine::wrongAnswer($userAnswer, (string)$correctAnswer, $name);
-                return;
-            }
-
-            Engine::correctAnswer();
+        if ($userAnswer !== (string)$correctAnswer) {
+            Engine\wrongAnswer($userAnswer, (string)$correctAnswer, $name);
+            return;
         }
 
-        Engine::congratulate($name);
+        Engine\correctAnswer();
     }
+
+    Engine\congratulate($name);
+}
+
+function gcd(int $a, int $b): int
+{
+    while ($b !== 0) {
+        $temp = $b;
+        $b = $a % $b;
+        $a = $temp;
+    }
+    return $a;
 }
