95ccc7b0245b3182ec886ce9ceff62cc74c80c1b
alexkosm87 <kosm24@yandex.ru>
2025-04-18 09:21:50 +0400

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -2,51 +2,49 @@
 
 namespace PhpProject45;
 
-class Engine
+function runGame(callable $gameLogic): void
 {
-    public static function run($game)
-    {
-        $game->run();
-    }
+    $name = getUserName();
+    welcome($name);
+    $gameLogic($name);
+}
 
-    public static function getUserName(): string
-    {
-        echo "Welcome to the Brain Games!\n";
-        echo "May I have your name? ";
-        $name = trim(fgets(STDIN));
-        return $name;
-    }
+function getUserName(): string
+{
+    echo "Welcome to the Brain Games!\n";
+    echo "May I have your name? ";
+    $name = trim(fgets(STDIN));
+    return $name;
+}
 
-    public static function welcome($name)
-    {
-        echo "Hello, $name!\n";
-        echo "What is the result of the expression?\n";
-    }
+function welcome(string $name): void
+{
+    echo "Hello, $name!\n";
+}
 
-    public static function askQuestion(string $question)
-    {
-        echo "Question: $question\n";
-        echo "Your answer: ";
-    }
+function askQuestion(string $question): void
+{
+    echo "Question: $question\n";
+    echo "Your answer: ";
+}
 
-    public static function getUserAnswer(): string
-    {
-        return trim(fgets(STDIN));
-    }
+function getUserAnswer(): string
+{
+    return trim(fgets(STDIN));
+}
 
-    public static function correctAnswer()
-    {
-        echo "Correct!\n";
-    }
+function correctAnswer(): void
+{
+    echo "Correct!\n";
+}
 
-    public static function wrongAnswer(string $userAnswer, string $correctAnswer, string $name)
-    {
-        echo "'$userAnswer' is wrong answer ;(. Correct answer was '$correctAnswer'.\n";
-        echo "Let's try again, $name!\n";
-    }
+function wrongAnswer(string $userAnswer, string $correctAnswer, string $name): void
+{
+    echo "'$userAnswer' is wrong answer ;(. Correct answer was '$correctAnswer'.\n";
+    echo "Let's try again, $name!\n";
+}
 
-    public static function congratulate(string $name)
-    {
-        echo "Congratulations, $name!\n";
-    }
+function congratulate(string $name): void
+{
+    echo "Congratulations, $name!\n";
 }
