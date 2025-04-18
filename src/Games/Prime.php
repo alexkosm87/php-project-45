9851f8458a7f5eec773986e1439738bdf8e16f62
alexkosm87 <kosm24@yandex.ru>
2025-04-18 09:22:43 +0400

    Update Prime.php

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -4,51 +4,45 @@ namespace PhpProject45\Games;
 
 use PhpProject45\Engine;
 
-class Prime
+function runPrimeGame(string $name): void
 {
-    private const ROUNDS = 3;
+    const ROUNDS = 3;
 
-    public function run(): void // Указан тип возвращаемого значения
-    {
-        $name = Engine::getUserName();
-        Engine::welcome($name);
-        echo "Answer \"yes\" if given number is prime. Otherwise answer \"no\".\n";
+    echo "Answer \"yes\" if given number is prime. Otherwise answer \"no\".\n";
 
-        for ($i = 0; $i < self::ROUNDS; $i++) {
-            $question = random_int(1, 100); // Генерируем случайное число от 1 до 100
-            $correctAnswer = $this->isPrime($question) ? 'yes' : 'no';
+    for ($i = 0; $i < ROUNDS; $i++) {
+        $question = random_int(1, 100);
+        $correctAnswer = isPrime($question) ? 'yes' : 'no';
 
-            Engine::askQuestion((string)$question); // Приводим к строке для совместимости
-            $userAnswer = Engine::getUserAnswer();
+        Engine\askQuestion($question);
+        $userAnswer = Engine\getUserAnswer();
 
-            if ($userAnswer !== $correctAnswer) {
-                Engine::wrongAnswer($userAnswer, $correctAnswer, $name);
-                return;
-            }
-
-            Engine::correctAnswer();
+        if ($userAnswer !== $correctAnswer) {
+            Engine\wrongAnswer($userAnswer, $correctAnswer, $name);
+            return;
         }
 
-        Engine::congratulate($name);
+        Engine\correctAnswer();
     }
 
-    // Метод для проверки, является ли число простым
-    public function isPrime(int $n): bool // Указан тип возвращаемого значения
-    {
-        if ($n < 2) {
+    Engine\congratulate($name);
+}
+
+function isPrime(int $n): bool
+{
+    if ($n < 2) {
+        return false;
+    }
+    if ($n === 2) {
+        return true;
+    }
+    if ($n % 2 === 0) {
+        return false;
+    }
+    for ($i = 3; $i <= sqrt($n); $i += 2) {
+        if ($n % $i === 0) {
             return false;
         }
-        if ($n === 2) {
-            return true; // 2 является простым числом
-        }
-        if ($n % 2 === 0) {
-            return false; // Четные числа больше 2 не являются простыми
-        }
-        for ($i = 3; $i <= sqrt($n); $i += 2) {
-            if ($n % $i === 0) {
-                return false; // Если n делится на i, то не простое
-            }
-        }
-        return true; // Если ни одно из условий не выполнено, то n простое
     }
+    return true;
 }
