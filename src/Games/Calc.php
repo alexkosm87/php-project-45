2f7a192ff5a32d5bc0f4bd3018ece51847935ed4
alexkosm87 <kosm24@yandex.ru>
2025-04-18 09:22:10 +0400

    Update Calc.php

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -4,38 +4,33 @@ namespace PhpProject45\Games;
 
 use PhpProject45\Engine;
 
-function runCalcGame(): void
+function runCalcGame(string $name): void
 {
     const OPERATIONS = ['+', '-', '*'];
     const ROUNDS = 3;
 
-    $name = Engine::getUserName();
-    Engine::welcome($name);
     echo "What is the result of the expression?\n";
 
     for ($i = 0; $i < ROUNDS; $i++) {
-        // Генерация вопроса
         $num1 = random_int(1, 50);
         $num2 = random_int(1, 50);
         $operation = OPERATIONS[array_rand(OPERATIONS)];
         $question = "$num1 $operation $num2";
 
-        // Вычисление правильного ответа
         $correctAnswer = getCorrectAnswer($num1, $num2, $operation);
 
-        Engine::askQuestion($question);
-        $userAnswer = Engine::getUserAnswer();
+        Engine\askQuestion($question);
+        $userAnswer = Engine\getUserAnswer();
 
-        // Приведение к строке для сравнения
         if ($userAnswer !== (string)$correctAnswer) {
-            Engine::wrongAnswer($userAnswer, (string)$correctAnswer, $name);
+            Engine\wrongAnswer($userAnswer, (string)$correctAnswer, $name);
             return;
         }
 
-        Engine::correctAnswer();
+        Engine\correctAnswer();
     }
 
-    Engine::congratulate($name);
+    Engine\congratulate($name);
 }
 
 function getCorrectAnswer(int $num1, int $num2, string $operation): int
