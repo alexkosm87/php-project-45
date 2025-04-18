f1454fc11f10a4f881c27c493a63e67b40757363
alexkosm87 <kosm24@yandex.ru>
2025-04-18 14:06:47 +0400

    Update Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,7 @@ namespace PhpProject45\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcome()
+function welcome(): void
 {
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
