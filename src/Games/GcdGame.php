5d8b6b5e192e1ea42aabd7d83a862d6ead3a3b22
alexkosm87 <kosm24@yandex.ru>
2025-04-18 19:32:01 +0400

    Update GcdGame.php

--- src/Games/GcdGame.php
+++ src/Games/GcdGame.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace PhpProject45\Games;
+namespace Games\GcdGame;
 
-use function PhpProject45\Engine\runGame;
+use function Engine\runGame;
 
 const GCD_DESCRIPTION = 'Find the greatest common divisor of given numbers.';
 
@@ -11,17 +11,16 @@ function gcd(int $a, int $b): int
     return $b === 0 ? $a : gcd($b, $a % $b);
 }
 
-function generateGcdQuestionAndAnswer(): array
-{
-    $num1 = random_int(1, 100);
-    $num2 = random_int(1, 100);
-    $question = "$num1 $num2";
-    $correctAnswer = (string) gcd($num1, $num2);
-
-    return [$question, $correctAnswer];
-}
-
 function runGcdGame(): void
 {
-    runGame(__NAMESPACE__ . '\generateGcdQuestionAndAnswer', GCD_DESCRIPTION);
+    $generateGcdQuestionAndAnswer = function (): array {
+        $num1 = random_int(1, 100);
+        $num2 = random_int(1, 100);
+        $question = "$num1 $num2";
+        $correctAnswer = (string) gcd($num1, $num2);
+
+        return [$question, $correctAnswer];
+    };
+
+    runGame($generateGcdQuestionAndAnswer, GCD_DESCRIPTION);
 }
