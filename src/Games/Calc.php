db57d54221c2a3f1f081504c653f65429e3eef03
alexkosm87 <kosm24@yandex.ru>
2025-04-18 11:47:12 +0400

    Update Calc.php

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -1,41 +1,39 @@
 <?php
 
-namespace PhpProject45\Games;
+namespace Games;
 
-use function PhpProject45\runGame;
+use Engine;
 
-function getRandomExpression()
+function getRandomOperation()
 {
     $operations = ['+', '-', '*'];
-    $num1 = rand(1, 50);
-    $num2 = rand(1, 50);
-    $operation = $operations[array_rand($operations)];
-
-    return [$num1, $num2, $operation];
+    return $operations[array_rand($operations)];
 }
 
 function calculate($num1, $num2, $operation)
 {
-    switch ($operation) {
-        case '+':
-            return $num1 + $num2;
-        case '-':
-            return $num1 - $num2;
-        case '*':
-            return $num1 * $num2;
+    if ($operation === '+') {
+        return $num1 + $num2;
+    } elseif ($operation === '-') {
+        return $num1 - $num2;
+    } elseif ($operation === '*') {
+        return $num1 * $num2;
     }
 }
 
-function playCalcGame()
+function getQuestionAndAnswer()
 {
-    [$num1, $num2, $operation] = getRandomExpression();
-    $question = "{$num1} {$operation} {$num2}";
+    $num1 = rand(1, 50);
+    $num2 = rand(1, 50);
+    $operation = getRandomOperation();
+
+    $question = "$num1 $operation $num2";
     $correctAnswer = calculate($num1, $num2, $operation);
 
-    return [$question, $correctAnswer];
+    return [$question, (string)$correctAnswer];
 }
 
-function startGame()
+function runGame()
 {
-    runGame('PhpProject45\Games\playCalcGame');
+    Engine\runGame('Games\getQuestionAndAnswer');
 }
