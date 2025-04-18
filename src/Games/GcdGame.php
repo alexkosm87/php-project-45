f5c40f23065ef876468d8ddaf14b9100c1650a81
alexkosm87 <kosm24@yandex.ru>
2025-04-18 14:01:35 +0400

    Update GcdGame.php
    random_int

--- src/Games/GcdGame.php
+++ src/Games/GcdGame.php
@@ -13,8 +13,8 @@ function gcd(int $a, int $b): int
 
 function generateGcdQuestionAndAnswer(): array
 {
-    $num1 = rand(1, 100);
-    $num2 = rand(1, 100);
+    $num1 = random_int(1, 100);
+    $num2 = random_int(1, 100);
     $question = "$num1 $num2";
     $correctAnswer = (string) gcd($num1, $num2);
 
